c609e0f794852198fb262dc993537067e67f9c4f
wesleymichael <wesleymichaelpereira@hotmail.com>
2023-05-24 15:55:44 -0300

    feat: encrypt password

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -1,11 +1,16 @@
 import { insertUser } from "../repository/users.repository.js";
+import bcrypt from "bcrypt";
 
 export async function signup(req, res){
     const {username, img, email, password} = req.body;
+
     try {
-        await insertUser(username, img, email, password);  
-        res.sendStatus(201)
+        const hash = bcrypt.hashSync(password, 10);
+        await insertUser(username, img, email, hash);  
+
+        return res.sendStatus(201)
     } catch (error) {
-        res.status(500).send(error.message);
+        return res.status(500).send(error.message);
     }
 }
+
