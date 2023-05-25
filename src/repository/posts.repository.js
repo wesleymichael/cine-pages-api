c247446a826c9b0bdbe5d7689a0e748ddfd62d9e
wesleymichael <wesleymichaelpereira@hotmail.com>
2023-05-25 10:27:16 -0300

    fix: query adjustment to get posts

--- src/repository/posts.repository.js
+++ src/repository/posts.repository.js
@@ -10,31 +10,44 @@ export async function insertPostDB(userId, description, img){
 
 export async function getPostsDB(userId){
     const results = await db.query(`
-        SELECT 
+        SELECT
             u.username,
             u."img" AS "userImg",
             json_build_object(
-                'id', p.id, 
-                'img', p.img, 
-                'description', p.description, 
-                'likes', COUNT(l.id),
-                'createAt', p."createdAt",
-                'liked', CASE WHEN l."userId" IS NOT NULL THEN true ELSE false END
+                'id', p.id,
+                'img', p.img,
+                'description', p.description,
+                'totalLikes', COUNT(l.id),
+                'liked', EXISTS (SELECT 1 FROM likes WHERE likes."userId" = $1 AND likes."postId" = p.id)
             ) AS post,
             json_build_object(
-                '1', COUNT(CASE WHEN r.stars = 1 THEN 1 END),
-                '2', COUNT(CASE WHEN r.stars = 2 THEN 1 END),
-                '3', COUNT(CASE WHEN r.stars = 3 THEN 1 END),
-                '4', COUNT(CASE WHEN r.stars = 4 THEN 1 END),
-                '5', COUNT(CASE WHEN r.stars = 5 THEN 1 END)
+                '1', COUNT(r.stars) FILTER (WHERE r.stars = 1),
+                '2', COUNT(r.stars) FILTER (WHERE r.stars = 2),
+                '3', COUNT(r.stars) FILTER (WHERE r.stars = 3),
+                '4', COUNT(r.stars) FILTER (WHERE r.stars = 4),
+                '5', COUNT(r.stars) FILTER (WHERE r.stars = 5)
             ) AS ratings
-        FROM 
+        FROM
             users u
             JOIN posts p ON u.id = p."userId"
-            LEFT JOIN likes l ON p.id = l."postId" AND l."userId" = $1
+            LEFT JOIN likes l ON p.id = l."postId"
             LEFT JOIN ratings r ON r."postId" = p.id
-        GROUP BY 
-            u.username, u."img", p.id, p.img, l."userId";
+        GROUP BY
+            u.username,
+            u."img",
+            p.id,
+            p.img,
+            p.description;
         `, [userId]);
     return results;
 }
+
+
+// export async function getPostsByUsernameDB(targetUsername, myId){
+//     const results = await db.query(`
+//         SELECT 
+//             u.username, u.img AS "userImg", 
+    
+    
+//     `)
+// }
--- src/routers/auth.router.js
+++ src/routers/auth.router.js
@@ -2,14 +2,13 @@ import { Router } from "express";
 import { logout, signin, signup } from "../controllers/auth.controllers.js";
 import { validadeSchema } from "../middlewares/validateSchema.middleware.js";
 import { signupSchema } from "../schemas/users.schemas.js";
-import { validateSignin } from "../middlewares/user.middleware.js";
 import { authValidation } from "../middlewares/authValidation.middleware.js";
 import { signinSchema } from "../schemas/auth.schemas.js";
 
 const authRouter = Router();
 
 authRouter.post("/signup", validadeSchema(signupSchema), signup);
-authRouter.post("/signin", validadeSchema(signinSchema), validateSignin, signin);
+authRouter.post("/signin", validadeSchema(signinSchema), signin);
 authRouter.post("/logout", authValidation, logout);
 
 export default authRouter;
